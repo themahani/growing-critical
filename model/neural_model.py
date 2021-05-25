4d0bd501e273a75e9b7283072e4de59ad28ba238
Ali Mahani <ali.a.mahani@zoho.com>
2021-05-25 15:17:43 +0430

    implement display to test system

--- model/neural_model.py
+++ model/neural_model.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
-class Neural_Network(object):
+class NeuralNetwork:
     """ the neural network model """
     def __init__(self, size, neuron_population):
         self.size = size    # the size of the area of the square canvas
@@ -15,7 +15,7 @@ class Neural_Network(object):
         self.dim = 2        # dimension of the canvas
         self.pos = np.random.random(size=(self.num, self.dim)) * self.size      # random initial position for the neuron in the canvas
 
-    
+
     def timestep(self):
         """ evolve the system one time step """
         pass
@@ -23,7 +23,22 @@ class Neural_Network(object):
 
     def calc_mutual_area(self):
         """
-        find the mutual area(2D) or volume (3D) of the neurons to find 
+        find the mutual area(2D) or volume (3D) of the neurons to find
         the interaction coefficients.
         """
         pass
+
+
+    def display(self):
+        """ plot the neurons on the canvas """
+        plt.plot(self.pos[:, 0], self.pos[:, 1], "bo", ms=0.5)
+        plt.show()
+
+def test():
+    """ function to test the system """
+    network = NeuralNetwork(100, 100)
+    network.display()
+
+
+if __name__ == '__main__':
+    test()
