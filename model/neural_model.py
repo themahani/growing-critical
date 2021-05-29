f89cb0a5f32005ea6fafcdea053fb8f54f1a708e
Ali Mahani <ali.a.mahani@zoho.com>
2021-05-30 01:04:46 +0430

    change data structure to store neuron info
    found bug in mutual_area

--- model/neural_model.py
+++ model/neural_model.py
@@ -14,9 +14,25 @@ class NeuralNetwork:
         self.size = size    # the size of the area of the square canvas
         self.num = neuron_population    # the population of the neuron in canvas
         self.dim = 2        # dimension of the canvas
-        self.pos = np.random.random(size=(self.num, self.dim)) * self.size      # random initial position for the neuron in the canvas
-        self.dist_mat = squareform(pdist(self.pos))     # distance matrix of the neurons
-        self.radii = np.random.random(size=self.num) * 0.05 * self.size    # list of neuron radii as disks (2D) or spheres (3D)
+        # hash table to store data of neurons: x, y, z, radius, fired
+        # x -> float
+        # y -> float
+        # z -> float
+        # radius -> float
+        self.neurons = np.recarray((self.num,),
+            dtype=[('x', float), ('y', float), ('z', float), ('radius', float)])
+
+        self.neurons['x'] = np.random.random(size=self.num) * self.size      # random initial position for the neuron in the canvas
+        self.neurons['y'] = np.random.random(size=self.num) * self.size      # random initial position for the neuron in the canvas
+
+        if self.dim == 3:
+            self.neurons['z'] = np.random.random(size=self.num) * self.size      # random initial position for the neuron in the canvas
+        else:
+            self.neurons['z'] = np.zeros(shape=self.num)    # all zero z-axis
+
+        self.neurons['radius'] = np.random.random(size=self.num) * 0.05 * self.size    # list of neuron radii as disks (2D) or spheres (3D)
+        pos = np.vstack((np.vstack((self.neurons['x'], self.neurons['y'])), self.neurons['z']))
+        self.dist_mat = squareform(pdist(pos.T))     # distance matrix of the neurons
         self.mutual_area = self.calc_mutual_area()      # calculate mutual area of disks(2D) or volume of spheres (3D)
 
     def timestep(self):
@@ -29,26 +45,28 @@ class NeuralNetwork:
         find the mutual area(2D) or volume (3D) of the neurons to find
         the interaction coefficients.
         """
-        mask = self.dist_mat < self.radii + self.radii.T    # find which neurons have intersections
-        _mutual_area = np.zeros(shape=(self.num, self.num))
-        r1 = np.tile(self.radii, (self.num, 1))      # matrix of radii
+        mask = self.dist_mat < self.neurons['radius'] + self.neurons['radius'].T    # find which neurons have intersections
+        _mutual_area = np.zeros(shape=(self.num, self.num)) # initialize
+        r1 = np.tile(self.neurons['radius'], (self.num, 1))      # matrix of radii
         r2 = r1.T           # transpose of radii as r2
         for i in range(self.size):
             for j in range(self.size):
-                if mask[i, j] == True:
+                if mask[i, j] == True and i != j:   # if has intersection and not self
                     _mutual_area[i, j] = 0.5 * \
                             np.sqrt((-self.dist_mat[i, j] + r1[i, j] + r2[i, j]) *\
                             (self.dist_mat[i, j] + r1[i, j] - r2[i, j]) *\
                             (self.dist_mat[i, j] - r1[i, j] + r2[i, j]) *\
                             (self.dist_mat[i, j] + r1[i, j] + r2[i, j]))
+        print("mutual area")
+        print(_mutual_area)
         return _mutual_area
 
 
     def display(self):
         """ plot the neurons on the canvas as disks """
-        r = self.radii
-        x = self.pos[:, 0]
-        y = self.pos[:, 1]
+        r = self.neurons['radius']
+        x = self.neurons['x']
+        y = self.neurons['y']
         phi = np.linspace(0.0,2*np.pi,100)
 
         na=np.newaxis
@@ -60,13 +78,14 @@ class NeuralNetwork:
 
         plt.plot(x_line,y_line,'b-')
         plt.title(f"neuron membrane of size {self.size} and population {self.num}")
+        plt.xlabel("x")
+        plt.ylabel("y")
         plt.show()
 
 
 def test():
     """ function to test the system """
     network = NeuralNetwork()
-    network.calc_mutual_area()
     network.display()
 
 
