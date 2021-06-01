c781c0c563cf58cbc413ce7fa7ceb9c2d9408ecc
Ali Mahani <ali.a.mahani@zoho.com>
2021-06-01 20:22:20 +0430

    add simple dynamics and animation

--- model/neural_model.py
+++ model/neural_model.py
@@ -10,7 +10,7 @@ from scipy.spatial.distance import squareform, pdist
 
 class NeuralNetwork:
     """ the neural network model """
-    def __init__(self, size=1, neuron_population=100):
+    def __init__(self, size=1, neuron_population=100, f0=0.01):
         self.size = size    # the size of the area of the square canvas
         self.num = neuron_population    # the population of the neuron in canvas
         self.dim = 2        # dimension of the canvas
@@ -20,7 +20,8 @@ class NeuralNetwork:
         # z -> float
         # radius -> float
         self.neurons = np.recarray((self.num,),
-            dtype=[('x', float), ('y', float), ('z', float), ('radius', float)])
+            dtype=[('x', float), ('y', float), ('z', float), ('radius', float),
+                ('f_i', float)])
 
         self.neurons['x'] = np.random.random(size=self.num) * self.size      # random initial position for the neuron in the canvas
         self.neurons['y'] = np.random.random(size=self.num) * self.size      # random initial position for the neuron in the canvas
@@ -35,9 +36,14 @@ class NeuralNetwork:
         self.dist_mat = squareform(pdist(pos.T))     # distance matrix of the neurons
         self.mutual_area = self.calc_mutual_area()      # calculate mutual area of disks(2D) or volume of spheres (3D)
 
+        self.neurons['f_i'] = f0    # initial firing rate
+
     def timestep(self):
         """ evolve the system one time step """
-        pass
+        _h = 10 ** -2       # defining timestep
+        r_dot0 = 10 ** -2
+
+        self.neurons['radius'] += r_dot0 * _h
 
 
     def calc_mutual_area(self):
@@ -70,7 +76,7 @@ class NeuralNetwork:
         return _mutual_area
 
 
-    def display(self):
+    def display(self, color):
         """ plot the neurons on the canvas as disks """
         r = self.neurons['radius']
         x = self.neurons['x']
@@ -84,17 +90,65 @@ class NeuralNetwork:
         x_line = x[na,:]+r[na,:]*np.sin(phi[:,na])
         y_line = y[na,:]+r[na,:]*np.cos(phi[:,na])
 
-        plt.plot(x_line,y_line,'b-')
+        plt.plot(x_line,y_line,f'{color}-')
         plt.title(f"neuron membrane of size {self.size} and population {self.num}")
         plt.xlabel("x")
         plt.ylabel("y")
         plt.show()
 
+    def animate_system(self):
+        def animate(i):
+            """ animate for FuncAnimation """
+            for _ in range(10):
+                self.timestep()
+
+            r = self.neurons['radius']
+            x_line = x[na,:]+r[na,:]*np.sin(phi[:,na])
+            y_line = y[na,:]+r[na,:]*np.cos(phi[:,na])
+            ax.clear()
+            ax.plot(x_line, y_line, 'b-')
+            plt.title(f"step {i}, neuron population = {self.num}")
+
+            return 0
+
+        r = self.neurons['radius']
+        x = self.neurons['x']
+        y = self.neurons['y']
+        phi = np.linspace(0.0,2*np.pi,100)
+
+        na=np.newaxis
+
+        # the first axis of these arrays varies the angle,
+        # the second varies the circles
+        x_line = x[na,:]+r[na,:]*np.sin(phi[:,na])
+        y_line = y[na,:]+r[na,:]*np.cos(phi[:,na])
+
+        fig, ax = plt.subplots()
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.plot(x_line, y_line, 'b-')
+
+        ax.set_xlim(0, self.size)
+        ax.set_ylim(0, self.size)
+
+        ani = FuncAnimation(fig, animate, interval=10, blit=False,
+                save_count=500)
+        plt.show()
+
+
 
 def test():
     """ function to test the system """
+    from time import time
     network = NeuralNetwork(neuron_population=50)
-    network.display()
+    start = time()
+    network.display('b')
+    for i in range(1000):
+        print(f"\rstep {i}", end='')
+        network.timestep()
+    print(f"runtime = {time() - start}")
+    network.display('r')
+
 
 
 if __name__ == '__main__':
