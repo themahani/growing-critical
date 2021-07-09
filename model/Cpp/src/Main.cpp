5ea44287ac24dfc71fb740bf5c5e3c8ea96e7abc
Ali Mahani <ali.a.mahani@zoho.com>
2021-07-09 23:07:04 +0430

    organize code, test run 4

--- model/Cpp/src/Main.cpp
+++ model/Cpp/src/Main.cpp
@@ -11,22 +11,22 @@ int main(void)
 {
     auto start = std::chrono::high_resolution_clock::now();
 
-    const char* run_name = "test_run3";
+    const char* run_name = "test_run4";
 
     /*
      * args:
      * network size: 1
      * population: 100
      * f0:  0.01
-     * time step: 0.001
+     * time step: 0.01
      * g: 500
-     * tau = dacay_time: 0.01
+     * tau = dacay_time: 0.1
      * f_sat: 2.0
-     * K = r_dot: 0.0001
+     * K = r_dot: 0.01
      */
-    NeuralNetwork mySystem = NeuralNetwork(1, 100, 0.01, 0.001, 500, 0.01, 2.0, 0.0001);
+    NeuralNetwork mySystem = NeuralNetwork(1, 100, 0.01, 0.01, 500, 0.1, 2.0, 0.01);
     mySystem.output_neuron_data(run_name);
-    mySystem.simulate_system(5000.0, run_name);
+    mySystem.simulate_system(50000.0, run_name);
 
     auto stop = std::chrono::high_resolution_clock::now();
 
