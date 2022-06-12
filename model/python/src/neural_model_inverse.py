a244b9341a63cf1dee2c0572c62b141b3f45a71b
Ali Mahani <ali.a.mahani@zoho.com>
2022-06-12 19:22:02 +0430

    change fire rate and radius update and evolution to be event-based

--- model/python/src/neural_model_inverse.py
+++ model/python/src/neural_model_inverse.py
@@ -14,7 +14,7 @@ class NeuralNetwork:
             f0: float=0.01, f_sat: float=2, g: float=500, k: float=10 ** -6,
             timestep: float=0.001) -> None:
         self.size = size    # the size of the area of the square canvas
-        self.num = neuron_population    # the population of the neuron in canvas
+        self._num = neuron_population    # the population of the neuron in canvas
         self.dim = 2        # dimension of the canvas
 
         """
@@ -25,19 +25,19 @@ class NeuralNetwork:
         radius -> float
         f_i -> float
         """
-        self.neurons = np.recarray((self.num,),
+        self.neurons = np.recarray((self._num,),
             dtype=[('x', float), ('y', float), ('z', float), ('radius', float),
                 ('f_i', float)])
 
-        self.neurons['x'] = np.random.random(size=self.num) * self.size      # random initial position for the neuron in the canvas
-        self.neurons['y'] = np.random.random(size=self.num) * self.size      # random initial position for the neuron in the canvas
+        self.neurons['x'] = np.random.random(size=self._num) * self.size      # random initial position for the neuron in the canvas
+        self.neurons['y'] = np.random.random(size=self._num) * self.size      # random initial position for the neuron in the canvas
 
         if self.dim == 3:
-            self.neurons['z'] = np.random.random(size=self.num) * self.size      # random initial position for the neuron in the canvas
+            self.neurons['z'] = np.random.random(size=self._num) * self.size      # random initial position for the neuron in the canvas
         else:
-            self.neurons['z'] = np.zeros(shape=self.num)    # all zero z-axis
+            self.neurons['z'] = np.zeros(shape=self._num)    # all zero z-axis
 
-        self.neurons['radius'] = np.random.random(size=self.num) * 0.05 * self.size    # list of neuron radii as disks (2D) or spheres (3D)
+        self.neurons['radius'] = np.random.random(size=self._num) * 0.05 * self.size    # list of neuron radii as disks (2D) or spheres (3D)
         pos = np.vstack((np.vstack((self.neurons['x'], self.neurons['y'])), self.neurons['z']))
         self.dist_mat = squareform(pdist(pos.T))     # distance matrix of the neurons
         self.mutual_area = self.calc_mutual_area()      # calculate mutual area of disks(2D) or volume of spheres (3D)
@@ -46,7 +46,7 @@ class NeuralNetwork:
         self.neurons['f_i'] = f0    # initial firing rate
         self.f_sat = f_sat      # f_sat (Hz)
 
-        self.fired = np.zeros(self.num, dtype=bool)     # see if neurons are fired
+        self.fired = np.zeros(self._num, dtype=bool)     # see if neurons are fired
         self.tau = tau     # decay constant for firing rates
         self._h = timestep      # time step
         self.g = g            # correlation coefficient of mutual area (Hz)
@@ -54,13 +54,16 @@ class NeuralNetwork:
         self.f_sat = f_sat
 
 
-        self.time_ax = np.arange(0, 500 ,timestep)  # initialize the time axis
+        self.MAX_TIME = 500   # the latest possible spike time in seconds
+        self.time_ax = np.arange(0, self.MAX_TIME ,timestep)  # initialize the time axis
         self.f_list = np.arange(0.01, 10, .1)   # list of various initial f
         cpdf_list = []  # initialize cpdf
         for f0 in self.f_list:
-            isi = NeuralNetwork.pdf(self.time_ax, tau=tau, f_0=self.f0, f0=f0)
-            cpdf_list.append(np.cumsum(isi) * self._h)
+            isi = NeuralNetwork.pdf(self.time_ax, tau=tau, f_0=self.f0, f0=f0)  # generate isi distribution using this f0
+            cpdf_list.append(np.cumsum(isi) * self._h)  # add the cpdf to list
         self.cpdf = np.array(cpdf_list)
+
+        self.current_time = 0   # initialize system time
 
 
     @staticmethod
@@ -115,40 +118,60 @@ class NeuralNetwork:
         ind: int
             index of the neuron that fires the next spike
         """
-        max_time = self.time_ax.shape() # initialize as the latest time possible
-        rand = np.random.uniform(0, 1, self.num)    # generate random for time
-        for i in range(self.num):   # do this for each neuron
+        min_time = self.MAX_TIME    # initialize as the latest time possible
+        rand = np.random.uniform(0, 1, self._num)    # generate random for time
+        neuron_ind = 0
+        for i in range(self._num):   # do the following for each neuron
             f_ind = NeuralNetwork.nearest_value(self.f_list,
                 self.neurons['f_i'][i]) # find the best f0 for f_i of this neuron
             rand_time_index = NeuralNetwork.nearest_value(self.cpdf[f_ind],
                 rand[i])    # find the next spike of neuron i
             if rand_time_index == None: # if rand_time greater than time axis, ignore it
                 continue
-            if rand_time_index < max_time:
-                max_time = rand_time_index  # keep the min time
+            if rand_time_index < min_time:
+                min_time = rand_time_index  # keep the min time
+                neuron_ind = i      # record the neuron that fired
 
-        return max_time * self._h
+        return min_time * self._h, neuron_ind
+
+    # def update_fire_rate(self):
+    #     """Update the firing rate of each neuron."""
+    #     # homogenious part
+    #     self.neurons['f_i'] += (self.f0 - self.neurons['f_i']) / self.tau * self._h
+    #     # inhomogenious part
+    #     if np.sum(self.fired) > 0:  # if at least 1 neuron fired
+    #         self.mutual_area = self.calc_mutual_area()  # update mutual area
+    #         self.neurons['f_i'][self.fired] += np.sum(self.mutual_area[self.fired]) * self.g    # inhomogenious increment of f_i
+    #     else:
+    #         pass
+
+    def _update_fire_rate(self, duration: float, fired_neuron: int):
+        """Update the firing rate of the neurons until the given duration"""
+        self.mutual_area = self.calc_mutual_area()  # update mutual area
+
+        """!!!The inhomogenious update might be wrong!!!"""
+        # inhomogenious update
+        self.neurons['f_i'][fired_neuron] += np.sum(self.mutual_area[fired_neuron]) * self.g
+        # homogenious update
+        self.neurons['f_i'] = self.f0 - (self.f0 - self.neurons['f_i']) \
+            * np.exp(-duration / self.tau)
+
+    def _update_radius(self, duration: float, fired_neuron: int) -> None:
+        """Update the radius of all neurons for the given duration"""
+        # shrink the fired neuron in one time step
+        self.neurons['radius'][fired_neuron] -= self.k / self.f_sat * self._h
+        # global evolution of firing rates
+        self.neurons['radius'] += self.k * duration
 
 
-    def update_fire_rate(self):
-        """Update the firing rate of each neuron."""
-        # homogenious part
-        self.neurons['f_i'] += (self.f0 - self.neurons['f_i']) / self.tau * self._h
-        # inhomogenious part
-        if np.sum(self.fired) > 0:  # if at least 1 neuron fired
-            self.mutual_area = self.calc_mutual_area()  # update mutual area
-            self.neurons['f_i'][self.fired] += np.sum(self.mutual_area[self.fired]) * self.g    # inhomogenious increment of f_i
-        else:
-            pass
-
-
-    def timestep(self):
-        """Evolve the system one time step."""
-        # decide which neurons fire at this timestep
-        self.fired = np.random.random(size=self.num) < self.neurons['f_i'] * self._h
-        self.update_fire_rate()     # update fire rate
-        self.neurons['radius'] += self.k * self._h   # homogenious increment
-        self.neurons['radius'][self.fired] -= self.k / self.f_sat   # inhomogenious decrement
+    def evolve(self, until: float):
+        """Evolve the system to the next spike."""
+        time_limit = self.current_time + until
+        while self.current_time < time_limit:
+            next_spike_time, fired_neuron = self._find_next_spike()
+            self._update_fire_rate(next_spike_time, fired_neuron)   # update the firing rate
+            self._update_radius(next_spike_time, fired_neuron)  # update radius
+            self.current_time += next_spike_time    # update system time
 
     @staticmethod
     def func(d, r1, r2) -> float:
@@ -182,11 +205,11 @@ class NeuralNetwork:
     def calc_mutual_area(self) -> np.ndarray:
         """Calculate the mutual area(2D) or volume (3D) of all neurons."""
 
-        _mutual_area = np.zeros(shape=(self.num, self.num),dtype=float) # initialize
-        r1 = np.tile(self.neurons['radius'], (self.num, 1))      # matrix of radii
+        _mutual_area = np.zeros(shape=(self._num, self._num),dtype=float) # initialize
+        r1 = np.tile(self.neurons['radius'], (self._num, 1))      # matrix of radii
         r2 = r1.T           # transpose of radii as r2
 
-        for i in range(self.num):
+        for i in range(self._num):
             for j in range(i):
                 _mutual_area[i, j] = NeuralNetwork.func(self.dist_mat[i, j],
                         r1[i, j], r2[i, j])
@@ -205,12 +228,12 @@ class NeuralNetwork:
         fig, ax = plt.subplots(figsize=(8, 8), tight_layout=True)
 
         neurons_circles = []
-        for i in range(self.num):
+        for i in range(self._num):
             neurons_circles.append(plt.Circle(xy=(x[i], y[i]), radius=r[i],
                 alpha=0.2, color=color))
             ax.add_patch(neurons_circles[i])
 
-        ax.set_title(f"neuron membrane of size {self.size} and population {self.num}")
+        ax.set_title(f"neuron membrane of size {self.size} and population {self._num}")
         ax.set_xlabel("x")
         ax.set_ylabel("y")
         ax.set_xlim(-0.5, self.size + 0.5)
@@ -220,14 +243,13 @@ class NeuralNetwork:
     def animate_system(self, color):
         def animate(i):
             """Animate for FuncAnimation."""
-            for _ in range(10):
-                self.timestep()
+            self.evolve(100)    # evolve the system for at least 100 seconds
 
             r = self.neurons['radius']
-            for ind in range(self.num):
+            for ind in range(self._num):
                 neurons_circles[ind].radius = r[ind]
 
-            ax.set_title(f"step {i}, neuron population = {self.num}")
+            ax.set_title(f"Current System Time = {self.current_time}, Neuron Population = {self._num}")
 
             return 0
 
@@ -238,17 +260,17 @@ class NeuralNetwork:
         fig, ax = plt.subplots(figsize=(8, 8), tight_layout=True)
 
         neurons_circles = []
-        for i in range(self.num):
+        for i in range(self._num):
             neurons_circles.append(plt.Circle(xy=(x[i], y[i]), radius=r[i],
                 alpha=0.2, color=color))
             ax.add_patch(neurons_circles[i])
 
-        # ax.set_title(f"neuron membrane of size {self.size} and population {self.num}")
+        # ax.set_title(f"neuron membrane of size {self.size} and population {self._num}")
         ax.set_xlabel("x")
         ax.set_ylabel("y")
         ax.set_xlim(-0.5, self.size + 0.5)
         ax.set_ylim(-0.5, self.size + 0.5)
-        ani = FuncAnimation(fig, animate, interval=10, blit=False,
+        ani = FuncAnimation(fig, animate, interval=1000, blit=False,
                 save_count=500)
         plt.show()
 
@@ -272,7 +294,7 @@ class NeuralNetwork:
         n_steps = int(duration // self._h)
         interval = 1000 # take a sample every interval
         leng = n_steps // interval  # the number of intervals to loop
-        arr = np.zeros(shape=(leng, self.num), dtype=float) # the sample
+        arr = np.zeros(shape=(leng, self._num), dtype=float) # the sample
         if progress:    # optional progress log
             start = time()
             for i in range(leng):
@@ -303,26 +325,6 @@ def test():
     from time import time   # to calc runtime of the program
     network = NeuralNetwork(neuron_population=100)
     network.animate_system('b')
-
-    start = time()
-    duration = 5 * 10 ** 2
-    num = int(duration // network._h)
-    interval = 100
-    array = np.zeros((num // interval, network.num))
-
-    for i in range(num // interval):
-        print(f"\rstep {i} / {num // interval}", end='')
-        for _ in range(interval):
-            network.timestep()
-        array[i] = np.sum(network.mutual_area, axis=0) * network.tau * network.g
-
-    print(f"runtime = {time() - start}")
-
-    network.display('r')
-
-    print(array[-1])
-    plt.plot(np.linspace(1, num // interval, num // interval), array)
-    plt.show()
 
 
 if __name__ == '__main__':
